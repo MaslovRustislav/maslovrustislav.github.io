419970f948dbff649c950c9a7e0eded5784daca4
MaslovRustislav <mrdante95@mail.ru>
2022-01-26 19:53:16 +0200

    lesson 6

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ class App{
 		document.body.appendChild( container );
         
         this.camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,0.1,100)
-        this.camera.position.set(0,0,4)
+        this.camera.position.set(0,2,4)
         
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0xaaaaaa);
-         
+        
+        const ambient = new THREE.HemisphereLight(0xffffff,0xbbbbff,0.3)
+        this.scene.add(ambient);
+
+        const light = new THREE.DirectionalLight();
+        light.position.set(0.2,0.1,1)
+        this.scene.add(light);
+
         this.renderer = new THREE.WebGLRenderer({antialias: true});
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth,window.innerHeight);
@@ -25,15 +32,20 @@ class App{
         this.mesh = new THREE.Mesh(geometry, material);
 
         this.scene.add(this.mesh);
+        
+        const controls = new OrbitControls(this.camera, this.renderer.domElement)
 
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
-        this.mesh.rotateY(0.01)
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
     
 	render( ) {   
+        this.mesh.rotateY(0.01)
         this.renderer.render( this.scene, this.camera );
     }
 }
